5309d61c2b2696b8756452e0451fbb037527bb98
Walker <skywalker2834@gmail.com>
2021-04-04 17:22:58 +0700

    Change line drawing algorithm, add draw rectangle and circle functions

--- JS/script.js
+++ JS/script.js
@@ -7,7 +7,7 @@ var canvas = document.getElementById('canvas'),
 
 function drawGrid() {
     // vẽ lưới tọa độ
-    ctx.fillStyle = '#000000'
+    ctx.fillStyle = '#000000';
     for(let i = 0; i < canvas.height; i += pSize) {
         for(let j = 0; j < canvas.width; j++) {
             ctx.fillRect(j, i, 1, 1);
@@ -52,21 +52,62 @@ function getPixelPos(a, b) {
 }
 
 function fillPixel(x, y) {
-    ctx.fillRect(x, y, pSize, pSize);
+    pos = getPixelPos(x, y);
+    ctx.fillRect(pos.x, pos.y, pSize, pSize);
 }
 
-function drawLine(x1, y1, x2, y2) {
-    console.log(arguments);
-    if(x1 > x2) {
-        [x1, y1, x2, y2] = [x2, y2, x1, y1];
-    }
-    dx = x2 - x1;
-    dy = y2 - y1;
+function drawLine(x0, y0, x1, y1) {
+    // DDA algorithm
+    const dx = x1 - x0,
+          dy = y1 - y0,
+          s  = Math.abs(dx) > Math.abs(dy) ? Math.abs(dx) : Math.abs(dy),
+          xi = dx * 1.0 / s,
+          yi = dy * 1.0 / s;
+    var x  = x0,
+        y  = y0;
 
-    for(let x = x1; x < x2; x++) {
-        y = y1 + dy * (x - x1) / dx;
-        pos = getPixelPos(x, y);
-        fillPixel(pos.x, pos.y);
+    fillPixel(x0, y0);
+
+    for (let i = 0; i < s; i++) {
+        x += xi;
+        y += yi;
+        fillPixel(x, y);
+    }
+}
+
+function drawRectangle(x, y, w, h) {
+    let x1 = x + w,
+        y1 = y + h;
+    drawLine(x, y, x1, y);
+    drawLine(x1, y, x1, y1);
+    drawLine(x1, y1, x, y1);
+    drawLine(x, y1, x, y);
+}
+
+function drawCircle(x0, y0, radius) {
+    // mid-point algorithm
+    var x = radius,
+        y = 0,
+        radiusError = 1 - x;
+
+    while (x >= y) {
+        fillPixel(x + x0, y + y0);
+        fillPixel(y + x0, x + y0);
+        fillPixel(-x + x0, y + y0);
+        fillPixel(-y + x0, x + y0);
+        fillPixel(-x + x0, -y + y0);
+        fillPixel(-y + x0, -x + y0);
+        fillPixel(x + x0, -y + y0);
+        fillPixel(y + x0, -x + y0);
+        y++;
+
+        if (radiusError < 0) {
+            radiusError += 2 * y + 1;
+        }
+        else {
+            x--;
+            radiusError += 2 * (y - x + 1);
+        }
     }
 }
 
@@ -99,13 +140,25 @@ window.addEventListener('mouseup', function(evt){
 });
 
 document.getElementById('testBtn').addEventListener('click', function(evt) {
+    ctx.fillStyle = '#' + Math.random().toString(16).slice(-6);
+
     drawLine(
         Math.random() * canvas.width,
         Math.random() * canvas.height,
         Math.random() * canvas.width,
         Math.random() * canvas.height
         );
-
+    let x = Math.random() * canvas.width,
+        y = Math.random() * canvas.height;
+    drawRectangle(
+        x, y,
+        Math.random() * (canvas.width),
+        Math.random() * (canvas.height)
+        );
+    drawCircle(
+        x, y,
+        Math.random() * canvas.height
+        );
 });
 
 document.getElementById('clearBtn').addEventListener('click', function(evt) {
